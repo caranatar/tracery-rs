5d3084026e94f512d7c4e82b0ac3945917176c00
Caranatar <caranatar@riseup.net>
2020-08-18 01:48:14 -0400

    Move Node out of Grammar

--- src/grammar.rs
+++ src/grammar.rs
@@ -41,48 +41,6 @@ impl Rule {
     }
 }
 
-/// Represents a part of a single expandable string
-///
-/// This is used to represent both the plain text, and the expandable text sections of a string.
-///
-/// # Example
-///
-/// ```ignore
-/// let nodes = vec![
-///     Node::Tag(Tag::new("one")),
-///     Node::Text(" is the loneliest number".into()),
-/// ];
-///
-/// assert_eq!(parser::parse_str("#one# is the loneliest number").unwrap(), nodes);
-/// ```
-#[derive(Debug, PartialEq, Clone)]
-pub enum Node {
-    Tag(Tag),
-    Text(String),
-}
-
-impl From<Tag> for Node {
-    fn from(tag: Tag) -> Node {
-        Node::Tag(tag)
-    }
-}
-
-impl From<String> for Node {
-    fn from(s: String) -> Node {
-        Node::Text(s)
-    }
-}
-
-impl Node {
-    pub fn tag(s: &str) -> Result<Node> {
-        Ok(Node::Tag(Tag::parse(s)?))
-    }
-
-    pub fn text(s: &str) -> Node {
-        Node::Text(s.to_string())
-    }
-}
-
 impl Default for Grammar {
     fn default() -> Grammar {
         let mut modifiers = BTreeMap::new();
@@ -271,19 +229,6 @@ impl Flatten for Rule {
             .map(|n| n.flatten(grammar, overrides))
             .collect::<Result<Vec<String>>>()?;
         Ok(parts.join(""))
-    }
-}
-
-impl Flatten for Node {
-    fn flatten(
-        &self,
-        grammar: &Grammar,
-        overrides: &mut BTreeMap<String, String>,
-    ) -> Result<String> {
-        match self {
-            Node::Tag(ref tag) => tag.flatten(grammar, overrides),
-            Node::Text(ref s) => s.flatten(grammar, overrides),
-        }
     }
 }
 
