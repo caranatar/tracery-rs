537aa385ce6198ccfa8731152d88835c7f7d7b57
Caranatar <caranatar@riseup.net>
2020-08-22 17:31:44 -0400

    Added tests for Node

--- src/node.rs
+++ src/node.rs
@@ -59,3 +59,30 @@ impl Flatten for Node {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn construction() -> Result<()> {
+        let tag = Tag::parse("#a#")?;
+        assert_eq!(Node::Tag(tag), Node::tag("#a#")?);
+        
+        let text = "abc".to_string();
+        assert_eq!(Node::Text(text), Node::text("abc"));
+
+        Ok(())
+    }
+    
+    #[test]
+    fn conversion() -> Result<()> {
+        let tag = Tag::parse("#a#")?;
+        assert_eq!(Node::Tag(tag.clone()), Node::from(tag));
+        
+        let text = "abc".to_string();
+        assert_eq!(Node::Text(text.clone()), Node::from(text));
+        
+        Ok(())
+    }
+}
